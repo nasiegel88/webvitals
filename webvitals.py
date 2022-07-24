4f893bb3e2dcd7b13af6e2c2b9a6fe4aa7dcef57
nasiegel88 <nasiegel@ucdavis.edu>
2022-07-23 20:10:56 -0700

    move login to scripts

--- webvitals.py
+++ webvitals.py
@@ -1,86 +1,18 @@
-import argparse, pathlib, os, time, getpass
+import argparse, pathlib, os, time
 
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
-from selenium.common.exceptions import NoSuchElementException
-
-from jproperties import Properties
 
 from argparse import ArgumentParser
+
 from webdriver_manager.firefox import GeckoDriverManager
 
 # Import custom modules
+from scripts import login
 from scripts import location
 from scripts import diarrhea_observations
 from scripts import weight
 from scripts import snomed
-
-def login():
-    '''
-    This function will log into webvitals. Create a `webvitals_config.properties` file with your username
-    and password if you want to avoid entering your login credentials each time the program is ran.
-    '''
-    key="webvitals_config.properties"
-
-    try:
-        f = open(key)
-        configs = Properties()
-
-        with open(key, 'rb') as config_file:
-            configs.load(config_file)
-
-        items_view = configs.items()
-        list_keys = []
-        print(f"'{key}' found!\n") 
-        for item in items_view:
-            list_keys.append(item[0])
-            
-        # Login attempt (1)
-        driver.find_element_by_name("ssousername").send_keys(configs.get("USERNAME")[0])
-        driver.find_element_by_name("password").send_keys(configs.get("PASSWORD")[0])
-        xpath="/html/body/center/table/tbody/tr[3]/td/font/input[1]"
-        driver.find_element_by_xpath(xpath).click()
-        
-        # Enter Webvitals
-        driver.find_element_by_xpath("/html/body/ul/li[1]/a").click()
-        
-        # Handle errors logging in
-        WebDriverWait(driver=driver, timeout=10).until(
-        lambda x: x.execute_script("return document.readyState === 'complete'"))
-        error_message = "Incorrect username or password."
-        errors = driver.find_elements_by_class_name("flash-error")
-
-        # Print errors optionally
-        if any(error_message in e.text for e in errors):
-            print("[!] Login failed!")
-        else:
-            print("[+] Login successful!\n")
-  
-    except IOError:
-        print('Enter correct username and password combo to continue')
-        count=0
-        while count < 3:
-            username = getpass.getpass('Enter Username:')
-            password = getpass.getpass('Enter Password:')
-            try:
-                driver.find_element_by_name("ssousername").send_keys(username)
-                driver.find_element_by_name("password").send_keys(password)
-                xpath="/html/body/center/table/tbody/tr[3]/td/font/input[1]"
-                driver.find_element_by_xpath(xpath).click()
-                
-                # Enter Webvitals
-                driver.find_element_by_xpath("/html/body/ul/li[1]/a").click()
-                print('Access granted\n')
-                time.sleep(2)
-                break
-            except NoSuchElementException:
-                print('Access denied. Try again.')
-                count += 1
-#     else:
-#         globals()['driver'] = driver
 
 # Create parser
 parser = ArgumentParser()
@@ -113,13 +45,7 @@ elif args.list is not None:
     query_list = [str(item) for item in args.list.split(',')]
 
 # Manually handle the default for "function"
-query = "location" if args.query is None else args.query
-
-# Start program
-print('')
-print('Launching Webvitals webscraper')
-print('')
-time.sleep(2)
+query = "location" if args.query is None else args.query 
 
 # Enter options
 options = Options()
@@ -127,13 +53,18 @@ options.add_argument("--headless")
 
 # Install Geckodriver
 driver = webdriver.Firefox(executable_path=GeckoDriverManager(version="v0.22.0").install(), options=options)
+print('')
 
-# Go to login page
-driver.get("https://davos.primate.ucdavis.edu/login/")
+# Log into website
+driver = login.login(driver)
 
-login() # Login to website
+# Start program
+print('')
+print('Launching Webvitals webscraper')
+print('')
+time.sleep(2)
 
-# Run function to scraped records from webvitals
+# Run function to scrape records from webvitals
 if args.query=='location':
     print('Running location query')
     time.sleep(2)
