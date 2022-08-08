b65cefc0368617a43f514d9501c7ab823c83c641
nasiegel88 <nasiegel@ucdavis.edu>
2022-08-08 15:58:30 -0700

    add conception module

---
+++ scripts/conception.py
@@ -0,0 +1,123 @@
+# Conception
+
+import selenium, os, time, datetime
+
+import pandas as pd
+from pandas.api.types import is_numeric_dtype
+import dateutil.parser as dparser
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import NoSuchElementException
+
+from tqdm import tqdm
+from webdriver_manager.firefox import GeckoDriverManager
+
+def conception(driver, query_list):
+    
+    '''
+    This module will pull conception information such as when a dam was pregnant, how many times they were pregnant, and the IDs of their offspring.
+    '''
+    
+    data = pd.DataFrame()
+    
+    with tqdm(total=len(query_list)) as progress_bar:
+        
+            for i in query_list:
+                
+                    # Go to conception in Webvitals
+                    driver=driver
+                    driver.find_element_by_name("query_input").send_keys(i)
+                    driver.find_element_by_name("submit").click()
+                    
+                    try:
+                        # Return to Animal Summary
+                        xpath="/html/body/table[1]/tbody/tr[3]/td/center/table[2]/tbody/tr/td[1]/a"
+                        driver.find_element_by_xpath(xpath).click()
+                        
+                    except NoSuchElementException:
+                        s = '''\
+                        Animal Number {animal_num} Not Found! 
+                    Please be sure you entered a valid animal ID.\
+                        '''.format(animal_num=i)
+                        print(s)
+                        sys.exit(1)
+                                            
+                    # Go to conception table
+                    xpath='/html/body/table[1]/tbody/tr[3]/td/center/table[2]/tbody/tr/td[4]/a'
+                    driver.find_element_by_xpath(xpath).click()
+                    
+                    try:
+                        # Extract html table
+                        xpath="/html/body/table[2]/tbody/tr/td[1]/center[1]/table"
+                        tableelement= (
+                            WebDriverWait(driver,2)
+
+                            .until(EC.visibility_of_element_located((By.XPATH, xpath)))
+                            .get_attribute('outerHTML')
+                        )        
+                        table = pd.read_html(str(tableelement))[0]
+
+                        # Add column to specify MMU number
+                        table['MMU'] = i
+                        first_column = table.pop('MMU')
+                        table.insert(0, 'MMU', first_column)
+                        
+                    except Exception as e:
+                        # Create empty table if one does not exist for animals 
+                        no_data =  {
+                            'Conception':[None],
+                            'Unnamed: 1':[None],
+                            'Offspring':[None],
+                            'Sex':[None],
+                            'Concept Date':[None],
+                            'Breed Type':[None],
+                            'Unnamed: 6':[None],
+                            'Sire':[None],
+                            'Unnamed: 8':[None],
+                            'Gest Days':[None],
+                            'Est':[None],
+                            'PregTerm Date':[None],
+                            'Delivery Type':[None],
+                            'Loc Date':[None],
+                            'Location':[None],
+                            'Death Type':[None],
+                            'Comment':[None]
+                        }
+                        table = pd.DataFrame(no_data)
+                        
+                    # Add column to specify MMU number
+                    table['MMU'] = i
+                    first_column = table.pop('MMU')
+                    table.insert(0, 'MMU', first_column)  
+                    
+                    # Append dataframes into one dataframe
+                    data = data.append(table, ignore_index=True)
+                
+                    # Log job progress
+                    progress_bar.update(1)
+                    
+                    # Reorder columns
+                    column_names = [
+                        'MMU','Conception','Unnamed: 1','Offspring','Sex','Concept Date',
+                        'Breed Type','Unnamed: 6','Sire','Unnamed: 8','Gest Days','Est','PregTerm Date',
+                        'Delivery Type','Loc Date','Location','Death Type','Comment']
+                    data = data.reindex(columns=column_names)
+                    
+        # Add object to namespace
+    df = data
+    globals()['df'] = df
+
+    # Export table
+    os.makedirs('data', exist_ok=True)
+    output='webvitals_query.csv'
+    timestr = time.strftime("data/%Y%m%d-%H%M%S")
+    absolute_path = os.path.abspath(timestr)
+    df.to_csv(f"{timestr}-conception_{output}")
+
+    print('')
+    file=f"{absolute_path}-conception_{output}"    
+    print(f"Output file is located at: '{file}'")
